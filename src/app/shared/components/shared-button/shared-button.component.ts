5745122d5c417165f972ee93cf5b3f12e81bf63a
Jwalczaak <j.walczaak@gmail.com>
2025-04-27 10:27:40 +0200

    Added the ability to add an icon to a shared-button, and select color of shared-button

--- src/app/shared/components/shared-button/shared-button.component.ts
+++ src/app/shared/components/shared-button/shared-button.component.ts
@@ -1,15 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component, input, output } from '@angular/core';
-import { ButtonType } from '../../types/button.type';
+import { ButtonColor, ButtonType } from '../../types/button.type';
 
 @Component({
   selector: 'app-shared-button',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './shared-button.component.html',
   styleUrl: './shared-button.component.scss',
 })
 export class SharedButtonComponent {
   buttonType = input.required<ButtonType>();
   buttonText = input.required<string>();
+  buttonColor = input.required<ButtonColor>();
+  iconName = input<string>();
   clickButton = output<void>();
 
   onButtonClick(): void {
